7d7ece7c6a0fa9f8c4b57e40a7dba4c7b94459cf
RobertABT <rbterrey@gmail.com>
2014-07-29 23:21:35 +0100

    Update houghcircles.py
    searches for circles of any radius in a given image on the hard-drive

--- houghcircles.py
+++ houghcircles.py
@@ -6,14 +6,13 @@ from tkFileDialog import askopenfilename
 
 Tk().withdraw()
 filename = askopenfilename()
-
 img = cv2.imread(filename,0)
 img = cv2.medianBlur(img,5)
 cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 cannyimg=cv2.Canny(img,100,200)	
 
 circles = cv2.HoughCircles(cannyimg,cv.CV_HOUGH_GRADIENT,1,20,
-				param1=50,param2=30,minRadius=140,maxRadius=200)
+				param1=50,param2=30,minRadius=0,maxRadius=0) # if circles are too small change min to any number (in pixels)
 if circles ==None:
 	print 'No circles'
 else:
